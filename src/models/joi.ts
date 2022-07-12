20e98270745590631deac6c1e029114e27b3e206
Estevam <estevamfurtado@gmail.com>
2022-07-12 15:23:57 -0300

    fix: schema

--- src/models/joi.ts
+++ src/models/joi.ts
@@ -4,7 +4,7 @@ const password = Joi.string().pattern(new RegExp('^[0-9]{4}$'));
 const cvv = Joi.string().pattern(new RegExp('^[0-9]{3}$'));
 const amount = Joi.number().min(0);
 const id = Joi.number().min(0);
-const type = Joi.string().valid('groceries', 'restaurants', 'transport', 'education', 'health');
+const type = Joi.string().valid('groceries', 'restaurant', 'transport', 'education', 'health');
 
 export const joiSchemas = {
     password,
